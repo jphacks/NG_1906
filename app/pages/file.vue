d769c1db6102188330deeeb6861697be386d6dcc
yokozawa <yoko6473@outlook.jp>
2019-10-19 12:00:09 +0900

    ファイル共有画面のUI実装
    ・ファイル共有画面の基礎となるUIを実装した
    ・ストアにファイル情報を保存する

---
+++ app/pages/file.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-row column justify-center align-center>
+    <v-col v-for="file in files.roomA" :key="file.id" md="3">
+      <v-card
+        class="mx-auto"
+        max-width="344"
+      >
+        <v-card-title>
+          <v-row justify="center">
+            <p class="display-1 text--primary mx-auto">
+              {{ file.name }}
+            </p>
+          </v-row>
+        </v-card-title>
+        <v-card-text>
+          <v-row justify="center">
+            <div>
+              <v-icon size="100px">
+                {{ file.icon }}
+              </v-icon>
+            </div>
+          </v-row>
+          <br>
+          <v-divider />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="deep-purple accent-4"
+          >
+            ダウンロード
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Files',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(['user', 'files'])
+  },
+  methods: {
+
+  }
+}
+</script>
+<style>
+.fixed_btn
+{
+  position: fixed;
+  bottom: 70px;
+  right: 50px;
+  padding: 6px 40px;
+}
+</style>
--- app/store/index.js
+++ app/store/index.js
@@ -2,8 +2,7 @@
 export const state = () => ({
   user: {
     name: '',
-    id: '',
-    icon: ''
+    id: ''
   },
   rooms: [
     {
@@ -11,19 +10,23 @@ export const state = () => ({
       icon: 'mdi-wechat',
       id: '222',
       name: '雑談部屋',
-      users: [{ name: 'ryo', id: '1111', icon: '' }]
+      users: [{ name: 'ryo', id: '1111' }]
     },
     {
       page: 'file',
       icon: 'mdi-folder-multiple',
       id: '333',
       name: 'ファイル島',
-      users: [{ name: 'ryo', id: '1111', icon: '' }]
+      users: [{ name: 'ryo', id: '1111' }]
     }
   ],
   chat: {
     roomA: [{ name: 'ryo', content: 'おはよう' }, { name: 'ken', content: 'おはよう' }],
     roomB: [{ name: 'ryo' }, { name: 'ken' }]
+  },
+  files: {
+    roomA: [{ name: '過去問A', icon: 'mdi-file-pdf-outline', id: '' }, { name: '練習問題A', icon: 'mdi-file-pdf-outline', id: '' }],
+    roomB: [{ name: '練習問題', icon: 'mdi-pdf' }]
   }
 })
 
