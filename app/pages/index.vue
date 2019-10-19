82d03b036c675fbe2edf4acce82eb1c495c9036e
yokozawa <yoko6473@outlook.jp>
2019-10-20 05:34:08 +0900

    ログイン画面実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-row column justify-center align-center>
-    <v-col>
-      <v-row>
-        <v-col md="2" />
-        <v-col md="4">
-          <img sizes="10" :src="logo">
-        </v-col>
-        <v-col md="4">
-          <v-text-field
-            v-model="userName"
-            color="white"
-            solo
-            :rules="nameRules"
-            :counter="MaxNameLength"
-            label="name"
-            required
-          />
-          <v-spacer />
-          <v-btn
-            @click="AppStart()"
-          >
-            START
-          </v-btn>
-        </v-col>
-        <v-col md="2" />
-      </v-row>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row column justify="center" align="center" style="height:50vw;">
+      <v-col md="4" sm="12" cols="12" class="ml-auto">
+        <img large :src="logo">
+      </v-col>
+      <v-col md="4" sm="12" cols="12" class="mr-auto">
+        <v-container fluid>
+          <v-row justify="center">
+            <v-col cols="12" sm="12" md="12">
+              <v-text-field
+                v-model="userName"
+                solo
+                :rules="nameRules"
+                :counter="MaxNameLength"
+                label="name"
+                required
+              />
+            </v-col>
+            <v-col cols="12" sm="12" md="12">
+              <v-btn
+                @click="AppStart()"
+              >
+                START
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-col md="2" xs="0" sm="0" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
