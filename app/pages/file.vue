1339350a52ce434aabaf439dbadc9b1f7073cabd
yokozawa <yoko6473@outlook.jp>
2019-11-02 10:19:19 +0900

    ファイル表示機能の実装
    アップロードしたファイルが表示される。

--- app/pages/file.vue
+++ app/pages/file.vue
@@ -5,72 +5,53 @@
         <v-card style="padding: 10px; margin-bottom: 10px; border: 3px dashed #333333;" outlined @dragleave.prevent="checkDrag($event, 'new', false)" @dragover.prevent="checkDrag($event, 'new', true)" @drop.prevent="onDrop">
           <div>
             <label>
-              {{ msg2 }}
+              {{ msg }}
               <input type="file" style="display:none" @change="onDrop">
             </label>
           </div>
         </v-card>
       </v-col>
     </v-row>
-    <v-row column justify-center align-center>
-      <div v-if="files != null">
-        <div v-for="(file, index) in files" :key="index">
-          <div class="columns">
-            <div class="column">
-              <figure class="image is-square">
-                <img :src="images[index]">
-              </figure>
-            </div>
-            <div class="column">
-              <p class="has-text-weight-semibold is-size-4 has-text-centerd">
-                {{ file.name }}
-              </p>
-            </div>
-            <div class="column">
-              <p class="has-text-weight-semibold is-size-4 has-text-centerd">
-                {{ file.type }}
-              </p>
-            </div>
-            <div class="column">
-              <p class="has-text-weight-semibold is-size-4 has-text-centerd">
-                {{ file.size.toLocaleString() }} Byte
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <v-col v-for="file in files[user.file]" :key="file.id" md="3">
+    <v-row justify-center align-center>
+      <v-col
+        v-for="(file, index) in files"
+        :key="index "
+        col="4"
+        sm="4"
+        md="3"
+        lg="2"
+      >
         <v-card
           class="mx-auto"
-          max-width="344"
+          height="150"
         >
-          <v-card-title>
-            <v-row justify="center">
-              <p class="display-1 text--primary mx-auto">
-                {{ file.name }}
-              </p>
-            </v-row>
-          </v-card-title>
-          <v-card-text>
-            <v-row justify="center">
-              <div>
-                <v-icon size="100px">
-                  {{ file.icon }}
-                </v-icon>
-              </div>
-            </v-row>
-            <br>
-            <v-divider />
-          </v-card-text>
+          <v-img
+            :src="images[index]"
+            height="100"
+          />
+
+          <!-- {{file.type}} -->
+          <div>
+            <p class="textOverFlow">
+              {{ file.name }}
+            </p>
+          </div>
+          <!--
           <v-card-actions>
-            <v-btn
-              text
-              color="deep-purple accent-4"
-            >
-              ダウンロード
+            <v-btn text>
+              Share
             </v-btn>
+
+            <v-btn
+              color="purple"
+              text
+            >
+              Explore
+            </v-btn>
+
+            <v-spacer />
           </v-card-actions>
+          -->
         </v-card>
       </v-col>
     </v-row>
@@ -87,24 +68,21 @@ export default {
   data () {
     return {
       isDrag: null,
-      msg1: 'ここにファイルをドロップ。または',
-      msg2: 'drop files here or click to upload',
+      msg: 'drop files here or click to upload',
       files: [],
       images: []
     }
   },
   computed: {
-    ...mapState(['user', 'files'])
+    ...mapState(['user'])
   },
   methods: {
     checkDrag (event, key, status) {
       this.isDrag = status ? key : null
       if (status) {
-        this.msg1 = 'ドラッグ中'
-        this.msg2 = 'ここにドロップしてください'
+        // action
       } else {
-        this.msg1 = 'ここにファイルをドロップ。または'
-        this.msg2 = 'ここをクリックして選択'
+        // action
       }
     },
     onDrop (event) {
@@ -119,14 +97,12 @@ export default {
         reader.onload = () => {
           // reader.resultはbase64にエンコードされたデータが入る
           this.images.push(reader.result)
-          alert(reader.result)
         }
         reader.readAsDataURL(fileList[i])
 
         this.files.push(fileList[i])
       }
-      this.msg1 = 'ドロップされました'
-      this.msg2 = 'ファイル数は' + fileList.length + 'です。'
+      this.msg = 'ファイル数は' + fileList.length + 'です。'
     }
   }
 }
@@ -138,5 +114,13 @@ export default {
   bottom: 70px;
   right: 50px;
   padding: 6px 40px;
+}
+
+.textOverFlow
+{
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
 }
 </style>
